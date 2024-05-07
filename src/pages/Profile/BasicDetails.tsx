088e81e59cca99b2bbc71cda8511a3e75d11a689
Md Asaduzaman <asadasad9282@gmail.com>
2024-05-07 21:55:43 +0600

    profile section update

--- src/pages/Profile/BasicDetails.tsx
+++ src/pages/Profile/BasicDetails.tsx
@@ -67,6 +67,19 @@ const BasicDetails = ({ profile }: any) => {
           />
         </div>
 
+        <div className="mb-4.5">
+          <label className="mb-2.5 block text-black dark:text-white">
+            Sponsor
+          </label>
+          <input
+            type="sponsor"
+            defaultValue={profile?.referralCode as string}
+            placeholder="Phone"
+            disabled
+            className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+          />
+        </div>
+
         <button className="flex px-7 mx-auto justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90">
           Submit
         </button>
