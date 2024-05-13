6e5518d1cce86d1a68e32593055a051a91c4758e
Md Asaduzaman <asadasad9282@gmail.com>
2024-05-13 19:08:06 +0600

    work on deshoard page

--- src/pages/Dashboard/TizaraTokenDashboard.tsx
+++ src/pages/Dashboard/TizaraTokenDashboard.tsx
@@ -7,6 +7,7 @@ import { FaUserCheck } from 'react-icons/fa6';
 import { PiPackage } from 'react-icons/pi';
 import axios from 'axios';
 import { getTizaraUserToken } from '../../hooks/getTokenFromstorage';
+import WelcomeSection from './WelcomeSection';
 
 interface ApiResponse {
   statusCode: number;
@@ -66,13 +67,104 @@ const BizTokenDashboard: React.FC = () => {
         console.error('Error fetching data:', error);
       }
     };
-    fetchData();
+    // fetchData();
   }, []);
   console.log(profile);
 
   return (
     <DefaultLayout>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+      <WelcomeSection />
+      <div className="grid grid-cols-1 lg:grid-cols-6 gap-y-5 lg:gap-5">
+        <div className="col-span-4">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-2 2xl:gap-7.5">
+            <Link to={'/'}>
+              <CardDataStats
+                title="Deposit Wallet"
+                total={`${
+                  profile?.wallet?.depositWallet
+                    ? profile?.wallet?.depositWallet
+                    : '00'
+                } TIZARA`}
+                // rate="0.95%"
+                // levelDown
+              >
+                <PiPackage className="text-2xl dark:text-white text-primary" />
+              </CardDataStats>
+            </Link>
+
+            <Link to={'/'}>
+              <CardDataStats
+                title="Deposit Wallet"
+                total={`${
+                  profile?.wallet?.depositWallet
+                    ? profile?.wallet?.depositWallet
+                    : '00'
+                } TIZARA`}
+                // rate="0.95%"
+                // levelDown
+              >
+                <PiPackage className="text-2xl dark:text-white text-primary" />
+              </CardDataStats>
+            </Link>
+
+            <Link to={'/'}>
+              <CardDataStats
+                title="Native Wallet"
+                total={`${
+                  profile?.wallet?.nativeWallet
+                    ? profile?.wallet?.nativeWallet
+                    : '00'
+                } TIZARA`}
+                // rate="0.95%"
+                // levelDown
+              >
+                <UserIcon />
+              </CardDataStats>
+            </Link>
+
+            <Link to={'/'}>
+              <CardDataStats
+                title="Deposit Wallet"
+                total={`${
+                  profile?.wallet?.depositWallet
+                    ? profile?.wallet?.depositWallet
+                    : '00'
+                } TIZARA`}
+                // rate="0.95%"
+                // levelDown
+              >
+                <PiPackage className="text-2xl dark:text-white text-primary" />
+              </CardDataStats>
+            </Link>
+          </div>
+        </div>
+
+        <div className="col-span-2  rounded-sm cursor-pointer border border-stroke bg-white py-5 px-6 shadow-default dark:border-strokedark dark:bg-boxdark">
+          <div className=" flex justify-between flex-col ">
+            <div className="flex flex-col gap-3">
+              <h4 className="text-title-sm font-bold text-black dark:text-white">
+                Token Name: Tizara
+              </h4>
+              <h4 className="text-title-sm font-bold text-black dark:text-white">
+                Blockchain: Binance
+              </h4>
+              <h4 className="text-title-sm font-bold text-black dark:text-white">
+                Total Supply: 2343423
+              </h4>
+              <h4 className="text-title-sm font-bold text-black dark:text-white">
+                Current Price: 300$
+              </h4>
+            </div>
+            <button className="ms-auto justify-end mt-10 w-fit px-10  rounded-md bg-secondary py-3 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
+              DEPOSIT
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="mt-10 grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <Link to={'/'}>
           <CardDataStats
             title="Deposit Wallet"
---
+++ src/pages/Dashboard/WelcomeSection.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const WelcomeSection = () => {
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row  gap-4 py-4 lg:place-items-center justify-between">
+        <div className="flex  lg:place-items-center flex-col lg:flex-row gap-3 ">
+          <img
+            className="w-40 h-40 rounded-full"
+            src="https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
+            alt=""
+          />
+          <div>
+            <h2 className="text-4xl font-semibold">Mr. Tom Cruise</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex gap-2">
+            <button className="items-center justify-center rounded-md bg-secondary py-3 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
+              DEPOSIT
+            </button>
+
+            <button className="items-center justify-center rounded-md bg-success py-3 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
+              Swap
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WelcomeSection;
