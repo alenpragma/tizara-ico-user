3391a460b1b66b1b0fb3868dd9d8d6d085dea123
Asaduzzaman14 <asadasad9282@gmail.com>
2024-09-06 00:52:55 +0600

    user can withdraw tizara

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,3 +1,1 @@
-export const fromWallet = '0x0133c9c6aAf0c347AeD5c14C0613e05596901EB3';
-export const domainName = 'api.tizara.co';
-export const contactAddress = '0x0a30F4BC6D57bA1Be0f1ac117de97E6D4889420d';
+ 
