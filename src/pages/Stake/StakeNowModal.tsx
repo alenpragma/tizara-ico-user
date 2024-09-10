bd825825d5fbabc51fd850651a75a54db95bd52b
Asaduzzaman14 <asadasad9282@gmail.com>
2024-09-10 11:10:44 +0600

    update

--- src/pages/Stake/StakeNowModal.tsx
+++ src/pages/Stake/StakeNowModal.tsx
@@ -251,6 +251,7 @@ export const StakeNowModal = ({
                   <PuffLoader className="mx-auto" color="#36d7b7" size={40} />
                 ) : (
                   <button
+                    disabled
                     className="btn flex justify-center rounded bg-primary py-2 px-6 font-medium text-gray hover:shadow-1"
                     type="submit"
                   >
